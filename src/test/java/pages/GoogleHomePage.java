3fbc3538d5d82829d50598ac656a68972d5d02ea
setdarovamerjen <setdarovamerjen@gmail.com>
2023-06-08 21:14:46 -0500

    Adding new Pages

---
+++ src/test/java/pages/GoogleHomePage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleHomePage {
+
+    public GoogleHomePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+   // @FindBy
+
+}
