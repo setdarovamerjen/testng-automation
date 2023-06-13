83ace37b5756abb382cbd69c80b64b291eab0b7d
Merjen Setdarova <125778116+setdarovamerjen@users.noreply.github.com>
2023-06-12 20:08:55 -0500

    Update Google.java
    Remove extra lines

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -7,18 +7,18 @@ import org.openqa.selenium.Keys;
 import utilities.ApplicationFlow;
 import utilities.Config;
 import utilities.Driver;
-
+//remove line
 public class Google {
-
+// remove line
     Faker faker=new Faker();
-
+//remove this line
     @Before
     public void setup(){
-
+//remove line
     }
     @Test
 public void test1(){
-
+//rl
         for (int i=1; i<=10; i++) {
             Driver.getDriver().get(Config.getValue("googleURL"));
             String testData=faker.lorem().word();
@@ -28,7 +28,7 @@ public void test1(){
             Assert.assertTrue(title.contains(testData));
         }
     }
-
+//rl
     @Test
     public void test2(){
         System.out.println("New line");
@@ -38,7 +38,7 @@ public void test1(){
     public void test3(){
         System.out.println("Mkjahsljdas");
     }
-
+//rl
     public void tear(){
         Driver.quit();
     }
